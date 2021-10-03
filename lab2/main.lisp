53df119e8385a2e38a379625806ba5c6ac154116
Makar Solomatin <makar.solomatin.5@yandex.ru>
2021-10-03 19:57:26 +0300

    Initial commit

---
+++ lab2/main.lisp
@@ -0,0 +1,21 @@
+(defun number-of-bytes (n)
+  (ceiling (integer-length n) 8))
+
+ (defun invert-if-lower-one (n)
+  (if (= 1 (logand n 1)) (logxor 12 n) n))
+
+
+(defvar x)
+
+(defparameter z (+ 1 x))
+
+(defun main ()
+  (dotimes (i (number-of-bytes x))
+    (let* (
+        (b (ldb (byte 8 (* i 8)) x))
+        (lower (ldb (byte 4 0) b))
+        (upper (ldb (byte 4 4) b))
+        (result (+ (invert-if-lower-one lower) (ash (invert-if-lower-one upper) 4))))
+      (setf x (dpb result (byte 8 (* i 8)) x)))))
+
+(print x)
