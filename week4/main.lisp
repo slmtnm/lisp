1e9bfea183250aff22458cc51a77f5f0e8aca27a
Makar Solomatin <makar.solomatin.5@yandex.ru>
2021-10-07 21:12:30 +0300

    Simplify redundant code

--- week4/main.lisp
+++ week4/main.lisp
@@ -1,23 +1,16 @@
-(defun integer-list-p (lst)
-  "Checks whether list consists only of integers"
-  (every #'integerp lst))
-
 (defun odd-list-p (lst)
   "Checks whether integer list consists only of odds"
-  (every #'oddp lst))
+  (every #'(lambda (x) (and (integerp x) (oddp x))) lst))
 
-(defun sum (lst &aux (sum 0))
-  "Sums list"
-  (dolist (l lst sum) (setf sum (+ sum l))))
+  (defun sum (lst)
+    "Sums list"
+    (apply #'+ lst))
 
 (defun replace-odd-lists-by-their-sum (lst)
   "Replaces all sublists, that consists only of odd integers, by their sum"
-  (cond
-    ((not (consp lst)) lst) ; atom or nil
-    ((integer-list-p lst) (if (odd-list-p lst) (sum lst) lst)) ; integer-only list
-    (t (let ((res (mapcar #'replace-odd-lists-by-their-sum lst))) ; mixed list (some integers, some sublists)
-         (if (and (integer-list-p res) (odd-list-p res))
-             (sum res)
-             res)))))
+  (if (consp lst)
+      (let ((res (mapcar #'replace-odd-lists-by-their-sum lst)))
+        (if (odd-list-p res) (sum res) res))
+      lst))
 
 (print (replace-odd-lists-by-their-sum (read)))
