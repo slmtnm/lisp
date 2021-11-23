c162dd5884c61cc90344aebcebc41498883e55aa
Makar Solomatin <makar.solomatin.5@yandex.ru>
2021-11-23 17:19:23 +0300

    Week9

--- week4/main.lisp
+++ week4/main.lisp
@@ -2,15 +2,11 @@
   "Checks whether integer list consists only of odds"
   (every #'(lambda (x) (and (integerp x) (oddp x))) lst))
 
-(defun sum (lst)
-  "Sums list"
-  (apply #'+ lst))
-
 (defun replace-odd-lists-by-their-sum (lst)
   "Replaces all sublists, that consists only of odd integers, by their sum"
   (if (consp lst)
       (let ((res (mapcar #'replace-odd-lists-by-their-sum lst)))
-        (if (odd-list-p res) (sum res) res))
+        (if (odd-list-p res) (apply #'+ res) res))
       lst))
 
 (print (replace-odd-lists-by-their-sum (read)))
